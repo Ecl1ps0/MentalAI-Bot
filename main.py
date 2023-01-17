ad3ed0fbf9bb1e7b1b0fd932a8182ab281dea84d
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-01-17 19:54:29 +0600

    reply_to is changed to send_message

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ bot = TeleBot("5730082173:AAEZs2nXmIDDDyIZjX_mhHmhCjLekCzOVTI")
 
 @bot.message_handler(commands=['start'])
 def say_hello(message: Message):
-    bot.reply_to(message, "Hi man!")
+    bot.send_message(message.chat.id, f"Welcome dear {message.from_user.first_name}! \n" +
+                     "I can establish your mood and mental state by your response on a simple question:\n" +
+                     "How was your day?\n" +
+                     "Don’t be afraid, you can be honest with me.\n" +
+                     "\n" +
+                     "I can also give you free advice based on your mood.")
 
 
 def main():
